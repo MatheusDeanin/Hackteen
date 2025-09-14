91afdfa633f9e57a4b8fcec7723e2ec3c2397ffc
Math200912 <matheusgames52626@gmail.com>
2025-09-14 19:49:37 -0300

    Update Final

--- JavaScript/paginainicial.js
+++ JavaScript/paginainicial.js
@@ -3,10 +3,12 @@ function direcionar() {
     window.location.href = "../index.html", "_blank";
 }
 
+// Conectar botão à pagina github.html
 function github(){
     window.location.href = "../github.html"
 }
 
+// Conectar botão à pagina contato.html
 function contato(){
     window.location.href = "../contato.html"
 }
