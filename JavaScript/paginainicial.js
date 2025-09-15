af192f023b3f161a666efb934014dd8edbcb5815
Matheus Ferreira Jordão <150194277+MatheusJordao12@users.noreply.github.com>
2025-09-15 14:56:40 -0300

    Update paginainicial.js

--- JavaScript/paginainicial.js
+++ JavaScript/paginainicial.js
@@ -1,14 +1,14 @@
 // Conectar botão à pagina index.html
 function direcionar() {
-    window.location.href = "../index.html", "_blank";
+    window.location.href = "./index.html", "_blank";
 }
 
 // Conectar botão à pagina github.html
 function github(){
-    window.location.href = "../github.html"
+    window.location.href = "./github.html"
 }
 
 // Conectar botão à pagina contato.html
 function contato(){
-    window.location.href = "../contato.html"
+    window.location.href = "./contato.html"
 }
